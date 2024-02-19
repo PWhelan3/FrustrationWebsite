9f414f2cf6b7489f81fe12216468aa9b5a604b21
Peter <pwhelanstudio@gmail.com>
2024-02-19 21:21:58 +0000

    Starting Navbar

---
+++ frustration-website/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <ul>
+                <li>
+                    <a href="/">Exit</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
