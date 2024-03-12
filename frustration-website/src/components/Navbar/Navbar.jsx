fec88fb276c9c730d1301e21c9e87e31595b4b75
Peter <pwhelanstudio@gmail.com>
2024-03-12 21:27:30 +0000

    Adding elements to nav

--- frustration-website/src/components/Navbar/Navbar.jsx
+++ frustration-website/src/components/Navbar/Navbar.jsx
@@ -4,10 +4,20 @@ import styles from "./Navbar.module.css";
 export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
+      <a className={styles.title} href="/">Exit</a>
         <div className={styles.container}>
             <ul className={styles.list}>
                 <li>
-                    <a className={styles.title} href="/">Exit</a>
+                    <a className={styles.link} href="/">I need Help!</a>
+                </li>
+                <li>
+                    <a className={styles.link} href="/">Menu</a>
+                </li>
+                <li>
+                    <a className={styles.link} href="/">About</a>
+                </li>
+                <li>
+                    <a className={styles.link} href="/">Toggle View</a>
                 </li>
             </ul>
         </div>
